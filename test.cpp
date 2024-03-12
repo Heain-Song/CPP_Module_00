b3f122a904899eeef2e6a88e77b84224d1a3720d
Hea In Song <heain.song328@gmail.com>
2024-03-12 22:59:43 +0100

    cpp exercise

---
+++ test.cpp
@@ -0,0 +1,103 @@
+
+/*
+Exercise 1 - Classes
+https://cplusplus.com/doc/oldtutorial/classes/
+*/
+
+// #include <iostream>
+// using namespace std;
+
+// class CRect{
+
+// 	public:
+// 	int	calc(int x, int y) {return (x*y);};
+// };
+
+
+// int main(void)
+// {
+// 	CRect rect;
+// 	cout << rect.calc(5,5);
+// 	return (0);
+
+/*
+Exercise 2 - Array of Objects
+https://www.geeksforgeeks.org/array-of-objects-in-c-with-examples/
+*/
+
+// #include <iostream>
+// using namespace std;
+
+// class Employee
+// {
+// 	int		id;
+// 	char	name[30];
+// 	public:
+// 	void	getdata();
+// 	void	putdata();
+// };
+
+// void	Employee::getdata()
+// {
+// 	cout << "Enter ID : ";
+// 	cin >> id;
+// 	cout << "Enter Name : ";
+// 	cin >> name;
+// }
+
+// void	Employee::putdata()
+// {
+// 	cout << id << " ";
+// 	cout << name << " ";
+// 	cout << endl;
+// }
+
+// int main()
+// {
+// 	Employee emp;
+// 	emp.getdata();
+// 	emp.putdata();
+// 	return 0;
+// }
+
+/*Exercise 3 - Class
+https://www.geeksforgeeks.org/c-classes-and-objects/?ref=ml_lbp
+*/
+
+// #include <iostream>
+// using namespace std;
+
+// class	Geeks
+// {
+// 	public:
+// 		string	geekname;
+// 		void	printname(){cout << "Geekname is : " << geekname;}
+// };
+
+// int main()
+// {
+// 	Geeks obj1;
+// 	obj1.geekname = "hesong";
+// 	obj1.printname();
+// 	return 0;
+// }
+
+/*
+Exercise 4 - How to include an array of class objects within another class as a member variable
+*/
+
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+
+
+};
+
+
+
+int main()
+{
+
+}
