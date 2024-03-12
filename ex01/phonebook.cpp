fb8f641aed0ed576efaeaa40e42305c5cd1bf81e
Hea In Song <heain.song328@gmail.com>
2024-03-12 22:58:39 +0100

    ex01 WIP

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <string>
+#include <climits>
 
 class PhoneBook
 {
-	//default private
-	//if public -> public:
-	//what can be inside? - variables, functions
-
 	/*
-	an array of Contact
+	an array of Contact, max 8
 	*/
-	// NO Dynamic allocation allowed
-
 
 };
 
-class Contact
+class Contact //stands for a phonebook contact
 {
 	/*
 	first name
@@ -24,15 +19,22 @@ class Contact
 	phone number
 	darkest secret
 	*/
-
+	public:
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	unsigned long long	phone_number; // the max is ULLONG_MAX in climits headerfile
+	std::string	darkest_secret;
 };
 
 int main(int argc, char **argv)
 {
-	if (argc > 1)
+	if (argc == 1)
 	{
-		std::string input = argv[1];
-		std::cout << input << std::endl;
+		/*Variable declarations*/
+		(void)argv;
+		std::string cmd;
+		//unsigned long long int max = ULLONG_MAX;
 
 		/*
 		Instantiated as an instance of the PhoneBook class.
@@ -40,44 +42,60 @@ int main(int argc, char **argv)
 
 		/*Instantiate as an instance of the Contact class as well.
 		*/
+		Contact contact1;
+		std::cout << "-------------" << std::endl;
+		std::cout << contact1.first_name;
+		std::cout << "-------------" << std::endl;
 
 		/*
 		Prompt to enter one of 3 commands - ADD, SEARCH, EXIT
 		*/
 
-		/*if ADD
-			- check empty fields -> can't have empty filds!!
-			- check if max 8 -> if 9th comes, replace the oldest one with the new one.
-			- Success -> Add the contact to the phonebook & waits for another one.
+		// std::cout << "Enter a command : ";
+		// std::cin >> cmd;
 
-		*/
+		// if (cmd == "ADD")
+		// {
+		// 	/*if ADD
+		// 	- check empty fields -> can't have empty filds!!
+		// 	- check if max 8 -> if 9th comes, replace the oldest one with the new one.
+		// 	- Success -> Add the contact to the phonebook & waits for another one.
+		// 	*/
 
-		/*else if SEARCH
-			- display the saved contacts as a list of 4 columns (index, first name, last name and nickname)
+		// 	//if (contact1.first_name == NULL || )
+		// }
+		// else if (cmd == "SEARCH")
+		// {
+		// 	/*else if SEARCH
+		// 	- display the saved contacts as a list of 4 columns (index, first name, last name and nickname)
 
-			- each columns length = 10 characters
-				-> If the text is longer than the column, it must be truncated
-				-> and the last displayable character must be replaced by a dot (’.’)
+		// 	- each columns length = 10 characters
+		// 		-> If the text is longer than the column, it must be truncated
+		// 		-> and the last displayable character must be replaced by a dot (’.’)
 
-			- use pipe | to seperate columns
+		// 	- use pipe | to seperate columns
 
-			- right aligned
+		// 	- right aligned
 
-			- prompt the user again for the index of the entry to display
-				-> If the index is out of range or wrong, define a relevant behavior.
-				-> Otherwise, display the contact information, one field per line.
-		*/
-
-		/* else if EXIT
-			- contacts delete
-				-> lost forever
-			- program quit
-		*/
-
-		/* else
-			- Any other input is discarded.
-
-		*/
+		// 	- prompt the user again for the index of the entry to display
+		// 		-> If the index is out of range or wrong, define a relevant behavior.
+		// 		-> Otherwise, display the contact information, one field per line.
+		// 	*/
+		// }
+		// else if (cmd == "EXIT")
+		// {
+		// 	/* else if EXIT
+		// 	- contacts delete
+		// 		-> lost forever
+		// 	- program quit
+		// 	*/
+		// }
+		// else
+		// {
+		// /* else
+		// 	- Any other input is discarded.
+		// */
+		// }
 	}
 	return 0;
 }
