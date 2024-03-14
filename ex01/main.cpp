6b6d60b0068cd01653cff12f9b0adbe2fa3f1f85
Hea In Song <heain.song328@gmail.com>
2024-03-14 20:06:57 +0100

    ex01 - WIP

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <climits>
+#include <iomanip>
 
 class Contact //stands for a phonebook contact
 {
@@ -52,89 +52,110 @@ int main(int argc, char **argv)
 		/*
 		Prompt to enter one of 3 commands - ADD, SEARCH, EXIT
 		*/
-
-		std::cout << "Enter a command : ";
-		getline(std::cin, cmd);
-		if (cmd == "ADD")
+		while (1)
 		{
-			/*if ADD
-			- check empty fields -> can't have empty filds!!
-			- check if max 8 -> if 9th comes, replace the oldest one with the new one.
-			- Success -> Add the contact to the phonebook & waits for another one.
+			std::cout << "Enter a command : ";
+			getline(std::cin, cmd);
+	
+			/*
+			Execute the program according to the command
 			*/
-			std::cout << "First name : "; // when input is empty/space, it falls into infinite loops..
-			getline(std::cin, phonebook.contacts[i].first_name);
-
-			std::cout << "Last name : "; 
-			getline(std::cin, phonebook.contacts[i].last_name);
-
-			std::cout << "Nickname : ";
-			getline(std::cin, phonebook.contacts[i].nickname);
-			
-			std::cout << "Phone number : ";
-			getline(std::cin, phonebook.contacts[i].phone_number);
-			
-			std::cout << "Darkest secret : ";
-			getline(std::cin, phonebook.contacts[i].darkest_secret);
-
-			//verify if the inputs are safely saved
-			std::cout << phonebook.contacts[i].first_name << std::endl;
-			std::cout << phonebook.contacts[i].last_name << std::endl;
-			std::cout << phonebook.contacts[i].nickname << std::endl;
-			std::cout << phonebook.contacts[i].phone_number << std::endl;
-			std::cout << phonebook.contacts[i].darkest_secret << std::endl;
-
-			/*if (phonebook.contacts[i].first_name == || phonebook.contacts[i].last_name == NULL || phonebook.contacts[i].nickname == NULL
-					|| phonebook.contacts[i].phone_number == NULL || phonebook.contacts[i].darkest_secret == NULL)
+		
+			if (cmd == "ADD")
 			{
-				std::cout << "Error: Can't have empty fields" << std::endl;	
-			}*/
-				
-		}
-		else if (cmd == "SEARCH")
-		{
-			/*else if SEARCH
-			- display the saved contacts as a list of 4 columns (index, first name, last name and nickname)
+				/*if ADD
+				- check empty fields -> can't have empty filds!!
+				- check if max 8 -> if 9th comes, replace the oldest one with the new one.
+				- Success -> Add the contact to the phonebook & waits for another one.
+				*/
+				if (i > 7)
+					i = 0;
+				if (i < 8)
+				{
+					std::cout << "First name : "; // when input is empty/space, it falls into infinite loops..
+					getline(std::cin, phonebook.contacts[i].first_name);
+	
+					std::cout << "Last name : ";
+					getline(std::cin, phonebook.contacts[i].last_name);
+		
+					std::cout << "Nickname : ";
+					getline(std::cin, phonebook.contacts[i].nickname);
+						
+					std::cout << "Phone number : ";
+					getline(std::cin, phonebook.contacts[i].phone_number);
+					
+					std::cout << "Darkest secret : ";
+					getline(std::cin, phonebook.contacts[i].darkest_secret);
+					
+					/*verify if the inputs are safely saved
+					std::cout << "-----Current Index is " << i << "-----" << std::endl;
+					std::cout << "Your first name is " << phonebook.contacts[i].first_name << std::endl;
+					std::cout << "Your last name is " << phonebook.contacts[i].last_name << std::endl;
+					std::cout << "Your nickname is " << phonebook.contacts[i].nickname << std::endl;
+					std::cout << "Your phone number is " << phonebook.contacts[i].phone_number << std::endl;
+					std::cout << "Your secret is " << phonebook.contacts[i].darkest_secret << std::endl;
+					*/
 
-			- each columns length = 10 characters
+					/*if (phonebook.contacts[i].first_name == || phonebook.contacts[i].last_name == NULL || phonebook.contacts[i].nickname == NULL
+							|| phonebook.contacts[i].phone_number == NULL || phonebook.contacts[i].darkest_secret == NULL)
+					{
+						std::cout << "Error: Can't have empty fields" << std::endl;	
+					}*/
+					i++;
+				}
+			}
+			if (cmd == "SEARCH")
+			{
+				/*else if SEARCH
+				- display the saved contacts as a list of 4 columns (index, first name, last name and nickname)
+
+				- each columns length = 10 characters
 				-> If the text is longer than the column, it must be truncated
 				-> and the last displayable character must be replaced by a dot (’.’)
 
-			- use pipe | to seperate columns
+				- use pipe | to seperate columns (done)
 
-			- right aligned
+				- right aligned (done)
 
-			- prompt the user again for the index of the entry to display
+				- prompt the user again for the index of the entry to display
 				-> If the index is out of range or wrong, define a relevant behavior.
 				-> Otherwise, display the contact information, one field per line.
-			*/
-			i = 0;
-			while (i++ < 8)
-			{
-				std::cout << i << "|";
-				std::cout << phonebook.contacts[i].first_name << "|";
-				std::cout << phonebook.contacts[i].last_name << "|";
-				std::cout << phonebook.contacts[i].nickname << "|";
+				*/
+				std::cout << std::right << std::setw(10) << "Index" << " | ";
+				std::cout << std::right << std::setw(10) << "First name" << " | ";
+				std::cout << std::right << std::setw(10) << "Last name" << " | ";
+				std::cout << std::right << std::setw(10) << "Nickname" << " | ";
 				std::cout << std::endl;
-			}
+				i = 0;
+				while (i < 8)
+				{
+					std::cout << std::right << std::setw(10) << i << " | ";
+					std::cout << std::right << std::setw(10) << phonebook.contacts[i].first_name << " | ";
+					std::cout << std::right << std::setw(10) << phonebook.contacts[i].last_name << " | ";
+					std::cout << std::right << std::setw(10) << phonebook.contacts[i].nickname << " | ";
+					std::cout << std::endl;
+					i++;
+				}
 
-			
-		}
-		else if (cmd == "EXIT")
-		{
-			/* else if EXIT
-			- contacts delete
-				-> lost forever
-			- program quit
-			*/
+			}
+			if (cmd == "EXIT")
+			{
+				/* else if EXIT
+				- contacts delete
+					-> lost forever
+				- program quit
+				*/
+				break;
+			}
+			/*else
+			{
+			 else
+				- Any other input is discarded.
+				std::cout << "Invalid Command" << std::endl;
+			}*/ 
+			}
 		}
 		else
-		{
-		/* else
-			- Any other input is discarded.
-		*/
-			std::cout << "Invalid Command" << std::endl;
-		}
-	}
+			std::cout << "Invalid argc" << std::endl;
 	return 0;
 }
